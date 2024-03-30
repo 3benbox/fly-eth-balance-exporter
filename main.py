6cb3d68dd2d29e3698d9c7b1e812d2c864b812c8
Ben Wilson <doubtingben@gmail.com>
2024-03-29 23:35:06 -0400

    feat: fly working

--- main.py
+++ main.py
@@ -151,4 +151,4 @@ if __name__ == "__main__":
         await task
         logger.info("Background task stopped")
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
